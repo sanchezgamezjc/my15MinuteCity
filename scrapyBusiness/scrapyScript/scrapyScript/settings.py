482bb56d8e592726a951767f38f83202f8a9c602
Jose Sanchez <josesanchez@MacBook-Air-de-Jose.local>
2023-09-12 15:59:12 +0200

    Act 12.09

--- scrapyBusiness/scrapyScript/scrapyScript/settings.py
+++ scrapyBusiness/scrapyScript/scrapyScript/settings.py
@@ -8,11 +8,11 @@ NEWSPIDER_MODULE = "scrapyScript.spiders"
 # Basic configuration
 #########################
 
-CONCURRENT_REQUESTS = 5
-DOWNLOAD_DELAY = 1
+CONCURRENT_REQUESTS = 1
+DOWNLOAD_DELAY = 5
 CONCURRENT_REQUESTS_PER_DOMAIN = 1
 CONCURRENT_REQUESTS_PER_IP = 1
-ROBOTSTXT_OBEY = False
+ROBOTSTXT_OBEY = True
 
 
 
@@ -37,7 +37,7 @@ ROTATING_PROXY_LIST = get_proxies()
 USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36"
 
 DEFAULT_REQUEST_HEADERS = {
-    'authority': 'https://guiaempresas.universia.es/',
+    'authority': 'https://www.informa.es',
     'upgrade-insecure-requests': '1',
     'sec-fetch-site': 'none',
     'sec-fetch-mode': 'navigate',
--- scrapyBusiness/scrapyScript/scrapyScript/spiders/business_spider.py
+++ scrapyBusiness/scrapyScript/scrapyScript/spiders/business_spider.py
@@ -3,8 +3,8 @@ import re
 import json
 
 class AuthorSpider(scrapy.Spider):
-    name = "business"
-    start_urls = ["https://guiaempresas.universia.es/localidad/BARCELONA/"]
+    name = "business_informa"
+    start_urls = ["https://www.informa.es/directorio-empresas/Localidad_BARCELONA.html"]
     scraped_data = []
 
     def parse(self, response):
@@ -15,24 +15,32 @@ class AuthorSpider(scrapy.Spider):
             return  # Exit the spider
         else:
             self.logger.warning("No CAPTCHA detected on the page.")
-
         
 
-        business_page_links = response.css('table.ranking_einf tr a::attr(href)').getall()
+        business_page_links = response.css("td.nom_empresa a ::attr(href)").getall()
         if not business_page_links:
             # Print a message when there are no more links to follow
             self.logger.info("No more links in this page")
         else:
             yield from response.follow_all(business_page_links, self.parse_business)
 
-        pagination_links = response.css("li.arrow a")
+        pagination_links = response.css("a.next")
         yield from response.follow_all(pagination_links, self.parse)
 
     def parse_business(self, response):
+
+        row = response.xpath('//tr[th[contains(text(), "CNAE")]]')
+        
+        if row:
+            # Extract the text from the specific <td> element
+            cnae_text = row.xpath('.//td/text()').get()
+        else:
+            cnae_text = None
+
         business_data = {
-            "name": response.css("h1.localbusiness::text").get(),
+            "name": response.css("td.localbusiness::text").get(),
             "address": response.css("span.street-address::text").get(),
-            "type": response.css("td.td_ficha_univ:contains(' - ')::text").get()
+            "type": cnae_text
         }
         self.scraped_data.append(business_data)
         yield business_data
